bef9e66176e26075468712d68ef7d7c854b2b546
wangyu <514835475@qq.com>
2019-08-31 15:39:12 +0800

    app

---
+++ src/js/detail.js
@@ -0,0 +1,78 @@
+import '../scss/detail.scss';
+
+import Header from '../components/header/index';
+import NewsFrame from '../components/news_frame/index';
+import Collector from '../components/collector/index';
+
+import tools from '../utils/tools';
+
+const header = new Header(),
+    newsFrame = new NewsFrame(),
+    collector = new Collector();
+
+const App = ($) => {
+    const $app = $('#app'),
+        $frameWrapper = $app.children('.frame-wrapper'),
+        target = JSON.parse(localStorage.getItem('target')),
+        newsUrl = tools.getUrlQueryValue('news_url') || target.url,
+        uniquekey = tools.getUrlQueryValue('uniquekey') || target.uniquekey;
+
+    let collections = JSON.parse(localStorage.getItem('collections')) || {},
+        collected = Boolean(collections[uniquekey]);
+
+
+    const init = () => {
+        render().then(bindEvent);
+    };
+
+    const render = () => {
+        return new Promise((resolve, reject) => {
+            _renderHeader();
+            _renderFrame(newsUrl);
+            _renderCollector(collected);
+            resolve();
+        });
+    };
+
+    const bindEvent = () => {
+        $('.collector').on('click', newsCollect);
+    };
+
+    const _renderHeader = () => {
+        $app.append(header.tpl({
+            title: '新闻详情',
+            showLeftIcon: true,
+            showRightIcon: false
+        }));
+    };
+
+    const _renderFrame = (newsUrl) => {
+        $app.append(newsFrame.tpl(newsUrl));
+        console.log(newsFrame.tpl(newsUrl));
+        console.log($frameWrapper);
+    };
+
+    const _renderCollector = (collected) => {
+        $app.append(collector.tpl(collected));
+       
+
+    };
+
+    function newsCollect () {
+        console.log(1);
+        if (collections[uniquekey]) {
+            delete collections[uniquekey];
+            collected = false;
+        } else {
+            collections[uniquekey] = JSON.parse(localStorage.getItem('target'));
+            collected = true;
+        }
+
+        localStorage.setItem('collections', JSON.stringify(collections));
+        collector.changeCollector(collected);
+    }
+
+    init();
+};
+
+App(Zepto);
